c415fadb10d1ed354a64304977695e5294e6211b
Ocawesome101 <ocawesome101@gmail.com>
2020-07-28 17:50:29 -0400

    fixed logger

--- nt/system32/ntoskrnl.lua
+++ nt/system32/ntoskrnl.lua
@@ -70,8 +70,8 @@ do
   nt.ki.gpu.setBackground(0x000000)
   function nt.ki.log(msg)
     for line in msg:gmatch("[^\n]+") do
-      if y > h - 1 then
-        y = h
+      if y > h - 2 then
+        y = h - 1
         nt.ki.gpu.copy(1, 1, w, h - 1, 0, -1)
         nt.ki.gpu.fill(1, h - 1, w, 1, " ")
       else
