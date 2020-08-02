be63d6190be38a245c4435c01f71e74666e26e2f
Ocawesome101 <ocawesome101@gmail.com>
2020-08-01 20:48:36 -0400

    edit improvements, need to figure out cursor pos

--- nt/system32/edit.lua
+++ nt/system32/edit.lua
@@ -31,7 +31,8 @@ local args = cmd.parse(...)
 local fname
 
 if args[1] then
-  fname = require("pathlib").resolve(args[1])
+  local drv
+  drv, fname = require("pathlib").resolve(args[1])
   local f = io.open(args[1])
   if not f then
     goto cont
@@ -45,39 +46,27 @@ else
   error("Missing file argument", 0)
 end
 ::cont::
+
+local bgcol = 0x0000AA
+local fgcol = 0xAAAAAA
 if gpu.getDepth() > 1 then
-  gpu.setBackground(0x0000DD)
+  gpu.setBackground(bgcol)
 else
+  bgcol = 0x000000
   gpu.setBackground(0x000000)  
 end
-gpu.setForeground(0xFFFFFF)
+gpu.setForeground(fgcol)
 gpu.fill(1, 1, w, h, " ")
-local cx, cy = 0, 1
+local cx, cy = 1, 2
 
 local function checkCursor()
-  if cx < 0 then
-    if cy > 1 and line > 1 then
-      cy = cy - 1
-      line = line - 1
-      cx = #buf[line]
-    end
-  end
-  if cy < 2 then
+  if cy < 3 then
     if line > 1 then
       scroll.h = scroll.h - 1
       line = line - 1
       if cx > #buf[line] then cx = #buf[line] end
     else
-      cy = 2
-    end
-  end
-  if cx > #buf[line] + 1 then
-    if line < #buf then
-      line = line + 1
-      cy = cy + 1
-      cx = 0
-    else
-      cx = #buf[line]
+      cy = 3
     end
   end
   if cy > h then
@@ -90,20 +79,30 @@ local function checkCursor()
   end
 end
 
-gpu.setBackground(0xBEBEBE)
+gpu.setBackground(fgcol)
 gpu.setForeground(0x000000)
 gpu.fill(1, 1, w, 1, " ")
 gpu.set(1, 1, "F1: Quit | F3: Save and Quit")
+gpu.setBackground(bgcol)
+gpu.setForeground(fgcol)
+gpu.fill(1, 2, w, h - 1, "\u{2500}")
+gpu.fill(1, 3, w, h - 2, "\u{2502}")
+gpu.set(1, 2, "\u{250C}")
+gpu.set(w, 2, "\u{2510}")
+gpu.setBackground(fgcol)
+gpu.setForeground(bgcol)
+local drv, path = require("pathlib").resolve(fname)
+gpu.set((w // 2 - #fname - 1), 2, " " .. fname:upper() .. " ")
 
 local function draw()
-  gpu.setBackground(gpu.getDepth() > 1 and 0x0000DD or 0x000000)
-  gpu.setForeground(0xFFFFFF)
-  for i=1, h - 1, 1 do
-    local set = (buf[i + scroll.h] or " "):sub(1 + scroll.w):gsub("\n", "")
-    set = set .. string.rep(" ", w - #set)
-    gpu.set(1, i + 1, set)
+  gpu.setBackground(bgcol)
+  gpu.setForeground(fgcol)
+  for i=1, h - 2, 1 do
+    local set = (buf[i + scroll.h] or " "):sub(1 + scroll.w, 1 + scroll.w + w - 3):gsub("\n", "")
+    set = set .. string.rep(" ", w - #set - 2)
+    gpu.set(2, i + 2, set)
   end
-  gpu.set(cx + 1, cy, "\u{2588}")
+  gpu.set(cx + 2, cy, "\u{2588}")
 end
 
 local handlers = {}
@@ -116,7 +115,8 @@ handlers[28] = function()
     table.insert(buf, line + 1, ins)
   end
   line = line + 1
-  cx, cy = 0, cy + 1
+  scroll.w = 0
+  cx, cy = 1, cy + 1
 end
 handlers[200] = function() -- Up
   if line > 1 then cy = cy - 1 line = line - 1 end
@@ -125,10 +125,16 @@ handlers[208] = function() -- Down
   if line < #buf then cy = cy + 1 line = line + 1 end
 end
 handlers[203] = function() -- Left
-  if cx > 0 then cx = cx - 1 end
+  if cx > 1 then cx = cx - 1 end
+  if cx == 1 and scroll.w > 0 then
+    scroll.w = scroll.w - 1
+  end
 end
 handlers[205] = function() -- Right
-  if cx < #buf[line] then cx = cx + 1 end
+  if cx < w - 2 and cx + scroll.w < #buf[line] then cx = cx + 1 end
+  if cx == w - 2 and cx + scroll.w <= #buf[line] then
+    scroll.w = scroll.w + 1
+  end
 end
 local run = true
 handlers[59] = function() -- F1
@@ -148,7 +154,7 @@ handlers[61] = function() -- F3
 end
 handlers[14] = function() -- Backspace
   buf[line] = buf[line]:sub(1, cx + scroll.w - 1) .. buf[line]:sub(cx + scroll.w + 1)
-  if cx > 0 then cx = cx - 1 end
+  handlers[203]()
 end
 
 while run do
@@ -160,7 +166,7 @@ while run do
       handlers[code]()
     elseif char >= 32 and char < 127 then
       buf[line] = buf[line]:sub(1, cx + scroll.w) .. string.char(char) .. buf[line]:sub(cx + scroll.w + 1)
-      cx = cx + 1
+      handlers[205]()
     end
   end
 end
